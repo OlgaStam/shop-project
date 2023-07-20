acae52b7748e1e98b27484ee36742056effacc4b
olamurm <olamykh@gmail.com>
2023-07-20 21:02:57 +0300

    как я решила задание

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//
+//элементы всегда возвращаем один элемент (много заворачиваем в один)
 //компоненты перепишем в виде стрелковой функции
 // для компонентов используют ПаскальКейс, для обычных функций кэмелКейс
 // вызываются как теги
 const App = () => {
     return (
-        <div>
+        <header>
             <h1>Hello React</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo suscipit reprehenderit aliquam, possimus repellendus
-                error esse provident sit quis quia architecto repellat adipisci
-                consectetur sed. Non et aspernatur rem dolorem!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Explicabo suscipit reprehenderit aliquam, possimus repellendus
-                error esse provident sit quis quia architecto repellat adipisci
-                consectetur sed. Non et aspernatur rem dolorem!
-            </p>
-        </div>
+        </header>
     )
 }
+const content = (
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            suscipit reprehenderit aliquam, possimus repellendus error esse
+            provident sit quis quia architecto repellat adipisci consectetur
+            sed. Non et aspernatur rem dolorem!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            suscipit reprehenderit aliquam, possimus repellendus error esse
+            provident sit quis quia architecto repellat adipisci consectetur
+            sed. Non et aspernatur rem dolorem!
+        </p>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <App />
+        {content}
     </React.StrictMode>
 )
