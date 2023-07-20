74247c498d5689987136c5b11748a3ee23f6c364
olamurm <olamykh@gmail.com>
2023-07-20 20:25:28 +0300

    примеры синтаксиса c JSX и без

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+//without JSX
+// const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+// root.render(<React.StrictMode>{title}</React.StrictMode>)
+
+// with JSX
+const h1 = <h1 id="title-2">Hello React.js</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{h1}</React.StrictMode>)
