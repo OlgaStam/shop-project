b0fc16ec0ea0b9f7e4ff1fb75bf8f1ae528e4f06
olamurm <olamykh@gmail.com>
2023-07-20 23:56:06 +0300

    destructuring props

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,12 +3,27 @@ type Props = {
     year: number
 }
 
-const Header = (props: Props) => {
+const Header = ({ title, year }: Props) => {
+    //пропсы - это объект, тоже можно деструктуризировать
+    // console.log(props)
     return (
         <h1>
-            Hello {props.title}, Year ={props.year}
+            {/* Hello {props.title}, Year ={props.year} */}
+            Hello {title}, Year ={year}
         </h1>
     )
 }
 
 export default Header
+
+const user = {
+    name: 'Jack',
+    age: 20,
+}
+//способ использования объекта без деструктурезации
+// const name = user.name
+// const name = user.age
+
+//способ использования объекта с деструктурезацией
+const { name, age } = user
+//пропсы - это объект, тоже можно деструктуризировать
