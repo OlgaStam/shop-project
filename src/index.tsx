f115563fcdbbace8631f2740b5930267abea47f8
olamurm <olamykh@gmail.com>
2023-07-20 20:51:33 +0300

    компоненты

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//without JSX
-const title = React.createElement('h1', { id: 'title' }, 'Hello React')
-
-// with JSX
-const h1 = (
-    <h1 id="title-2" className="title">
-        Hello React.js
-    </h1>
-)
-const content = (
-    <div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            quibusdam blanditiis nisi hic porro iusto laudantium officiis
-            provident, at eos, distinctio fugit voluptatibus quasi ab
-            laboriosam. Magnam assumenda minus est?
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            quibusdam blanditiis nisi hic porro iusto laudantium officiis
-            provident, at eos, distinctio fugit voluptatibus quasi ab
-            laboriosam. Magnam assumenda minus est?
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            quibusdam blanditiis nisi hic porro iusto laudantium officiis
-            provident, at eos, distinctio fugit voluptatibus quasi ab
-            laboriosam. Magnam assumenda minus est?
-        </p>
-    </div>
-)
-
+//
+//компоненты перепишем в виде стрелковой функции
+// для компонентов используют ПаскальКейс, для обычных функций кэмелКейс
+// вызываются как теги
+const App = () => {
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo suscipit reprehenderit aliquam, possimus repellendus
+                error esse provident sit quis quia architecto repellat adipisci
+                consectetur sed. Non et aspernatur rem dolorem!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo suscipit reprehenderit aliquam, possimus repellendus
+                error esse provident sit quis quia architecto repellat adipisci
+                consectetur sed. Non et aspernatur rem dolorem!
+            </p>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        {h1}
-        {title}
-        {content}
+        <App />
     </React.StrictMode>
 )
