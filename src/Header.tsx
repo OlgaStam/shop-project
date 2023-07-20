6d31bd7e75b3d91f315be00de8455d8e11978edb
olamurm <olamykh@gmail.com>
2023-07-21 01:14:10 +0300

    добавили библиотеку sass

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,14 +16,14 @@ const Header = ({ title, year }: Props) => {
 
 export default Header
 
-const user = {
-    name: 'Jack',
-    age: 20,
-}
+// const user = {
+//     name: 'Jack',
+//     age: 20,
+// }
 //способ использования объекта без деструктурезации
 // const name = user.name
 // const name = user.age
 
 //способ использования объекта с деструктурезацией
-const { name, age } = user
+// const { name, age } = user
 //пропсы - это объект, тоже можно деструктуризировать
