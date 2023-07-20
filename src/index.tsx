a2d99e8495f92232a8771a95f0e855de43d21665
olamurm <olamykh@gmail.com>
2023-07-20 21:09:49 +0300

    как было надо решить задание

--- src/index.tsx
+++ src/index.tsx
@@ -5,34 +5,39 @@ import ReactDOM from 'react-dom/client'
 //компоненты перепишем в виде стрелковой функции
 // для компонентов используют ПаскальКейс, для обычных функций кэмелКейс
 // вызываются как теги
-const App = () => {
+const Header = () => {
+    return <h1>Hello React</h1>
+}
+
+const Content = () => {
     return (
-        <header>
-            <h1>Hello React</h1>
-        </header>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo suscipit reprehenderit aliquam, possimus repellendus
+                error esse provident sit quis quia architecto repellat adipisci
+                consectetur sed. Non et aspernatur rem dolorem!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo suscipit reprehenderit aliquam, possimus repellendus
+                error esse provident sit quis quia architecto repellat adipisci
+                consectetur sed. Non et aspernatur rem dolorem!
+            </p>
+        </div>
     )
 }
-const content = (
-    <div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-            suscipit reprehenderit aliquam, possimus repellendus error esse
-            provident sit quis quia architecto repellat adipisci consectetur
-            sed. Non et aspernatur rem dolorem!
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-            suscipit reprehenderit aliquam, possimus repellendus error esse
-            provident sit quis quia architecto repellat adipisci consectetur
-            sed. Non et aspernatur rem dolorem!
-        </p>
-    </div>
-)
-
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Content />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <App />
-        {content}
     </React.StrictMode>
 )
