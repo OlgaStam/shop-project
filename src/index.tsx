0f52bc3af3d909fbb85955ddea51567ee22f4a93
olamurm <olamykh@gmail.com>
2023-07-20 20:37:23 +0300

    всегда возвращаем один элемент (много заворачиваем в один)

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 //without JSX
-// const title = React.createElement('h1', { id: 'title' }, 'Hello React')
-
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// root.render(<React.StrictMode>{title}</React.StrictMode>)
+const title = React.createElement('h1', { id: 'title' }, 'Hello React')
 
 // with JSX
-const h1 = <h1 id="title-2">Hello React.js</h1>
+const h1 = (
+    <h1 id="title-2" className="title">
+        Hello React.js
+    </h1>
+)
+const content = (
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            quibusdam blanditiis nisi hic porro iusto laudantium officiis
+            provident, at eos, distinctio fugit voluptatibus quasi ab
+            laboriosam. Magnam assumenda minus est?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            quibusdam blanditiis nisi hic porro iusto laudantium officiis
+            provident, at eos, distinctio fugit voluptatibus quasi ab
+            laboriosam. Magnam assumenda minus est?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            quibusdam blanditiis nisi hic porro iusto laudantium officiis
+            provident, at eos, distinctio fugit voluptatibus quasi ab
+            laboriosam. Magnam assumenda minus est?
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{h1}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        {h1}
+        {title}
+        {content}
+    </React.StrictMode>
+)
