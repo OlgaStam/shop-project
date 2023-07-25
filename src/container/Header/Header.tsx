0473253b882b43b53cbe87497121a4c7ddb899af
olamurm <olamykh@gmail.com>
2023-07-25 20:26:55 +0300

    вынесли логику в другой компонент,  Menu

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,14 +1,13 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import './header.scss'
 import '../../components/Logo/Logo'
 import Logo from '../../components/Logo/Logo'
-
+import Menu from 'components/Menu/Menu'
 type Props = {}
 
 const Header = (props: Props) => {
@@ -28,11 +27,14 @@ const Header = (props: Props) => {
                     <Typography
                         variant="h6"
                         component="div"
-                        sx={{ flexGrow: 1 }}
+                        sx={{
+                            flexGrow: 1,
+                            marginTop: '8px',
+                        }}
                     >
                         <Logo />
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Menu />
                 </Toolbar>
             </Container>
         </AppBar>
