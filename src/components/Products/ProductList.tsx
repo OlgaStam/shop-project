b68409c48133a64777024ee7c1547a6fe1ebb8b0
olamurm <olamykh@gmail.com>
2023-07-26 14:01:59 +0300

    пропсы для  вида айфона

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -20,13 +20,13 @@ const ProductList = (props: Props) => {
             </Typography>
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem phoneType="14 Pro" />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem phoneType="15 Pro" />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
+                    <ProductListItem phoneType="16 Pro" />
                 </Grid>
             </Grid>
         </>
