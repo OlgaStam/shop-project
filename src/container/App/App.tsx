94ee2dbd3e92380bf2c866516bcd09591944349f
OlgaStam <newrealityforme@gmail.com>
2023-08-13 17:37:51 +0300

    написали addProductToCart , проверяем кнопкой в Арр

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -11,13 +11,22 @@ type CartDataProps = {
 }
 const App = (props: Props) => {
     const [cartData, setCartData] = useState<CartDataProps>({
-        totalCount: 10,
-        totalPrice: 100,
+        totalCount: 0,
+        totalPrice: 0,
     })
+
+const addProductToCart =(count: number, price:number)=>{
+    setCartData((prevState)=>({
+        totalCount: prevState.totalCount+count,
+        totalPrice: prevState.totalPrice+price*count
+    }))
+}
+
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
+            <button onClick={()=> addProductToCart(5,500)}>add to cart (count:5, prise:500$)</button>
             <Main />
         </StyledEngineProvider>
     )
