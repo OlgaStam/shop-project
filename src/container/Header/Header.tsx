03b87f27a1fe471685714e063595ea568e4d132d
OlgaStam <newrealityforme@gmail.com>
2023-08-13 19:28:21 +0300

    прописали структуру для динамичного отображения в корзине + уникальный ключ для каждого дива

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -11,13 +11,12 @@ import Menu from 'components/Menu/Menu'
 import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {
-    cartData:{
-        totalCount: number
-        totalPrice: number
+    productsInCart: {
+        [id: number]: number
     }
 }
 
-const Header = ({cartData}: Props) => {
+const Header = ({ productsInCart }: Props) => {
     return (
         <AppBar position="static" className="app-bar">
             <Container>
@@ -42,7 +41,7 @@ const Header = ({cartData}: Props) => {
                         <Logo />
                     </Typography>
                     <Menu />
-                    <CartHeader cartData={cartData} />
+                    <CartHeader productsInCart={productsInCart} />
                 </Toolbar>
             </Container>
         </AppBar>
