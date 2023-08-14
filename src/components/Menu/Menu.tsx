dc16c2454af40a6fbb9140c4a74e916ae67ecc5f
OlgaStam <newrealityforme@gmail.com>
2023-08-14 22:00:50 +0300

    NavLink className, стилизация

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,20 @@
 import { Button } from '@mui/material'
 import { NavLink } from 'react-router-dom'
+import './MenuItem.scss'
+
 type Props = {}
 const Menu = (props: Props) => {
     return (
         <>
             <Button color="inherit">
-                <NavLink to="/">Home</NavLink>
+                <NavLink
+                    className={({ isActive }) =>
+                        isActive ? 'menu-item-active' : 'menu-item'
+                    }
+                    to="/"
+                >
+                    Home
+                </NavLink>
             </Button>
             <Button color="inherit">About us</Button>
             <Button color="inherit">Payment</Button>
