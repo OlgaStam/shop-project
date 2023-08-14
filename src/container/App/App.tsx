a20f3e32e355577dbec9334a4bd7e40dfbba3079
OlgaStam <newrealityforme@gmail.com>
2023-08-14 19:35:37 +0300

    title вместо id в хедере - 1 способ

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -26,7 +26,7 @@ const App = (props: Props) => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-                        <Main addProductToCart={addProductToCart} />
+            <Main addProductToCart={addProductToCart} />
         </StyledEngineProvider>
     )
 }
