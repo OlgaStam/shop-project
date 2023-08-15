0fd9053d36d273d36e5b9b79d03b547a4c144b32
OlgaStam <newrealityforme@gmail.com>
2023-08-15 16:08:54 +0300

    кнопки +- работают в расширенной корзине

--- src/components/Cart/CartProductListItemExtended.tsx
+++ src/components/Cart/CartProductListItemExtended.tsx
@@ -1,26 +1,20 @@
-// type Props = {}
-
-// const CartProductListItemExtended = (props: Props) => {
-//   return (
-//     <div>CartProductListItemExtended</div>
-//   )
-// }
-
-// export default CartProductListItemExtended
 import { Button, Card, CardContent, Grid } from '@mui/material'
 import { Product } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Quantity from 'components/Quantity/Quantity'
 
 type Props = {
     product: Product
     productCount: number
     removeProductFromCart: (id: number) => void
+    changeProductQuantity: (id: number, count:number) => void
 }
 
 const CartProductListItemExtended = ({
     product,
     productCount,
     removeProductFromCart,
+    changeProductQuantity
 }: Props) => {
     return (
         <Grid item xs={12} sm={4}>
@@ -39,6 +33,24 @@ const CartProductListItemExtended = ({
                     <div className="product-features">
                         Count: {productCount}
                     </div>
+                    <Quantity
+                        count={productCount}
+                        onDecrementClick={() => {
+                            if (productCount <= 1) {
+                                removeProductFromCart(product.id)
+                            }
+                            else {
+                                changeProductQuantity( 
+                                    product.id,
+                                    productCount - 1
+                                )
+                            }
+                        }} 
+                        onIncrementClick={() =>
+                            changeProductQuantity(product.id, productCount + 1)
+                        }
+                        // minCount={0}
+                    />
                     <Button
                         variant="outlined"
                         onClick={() => removeProductFromCart(product.id)}
