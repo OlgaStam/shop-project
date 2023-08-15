09aa886e9eb3368971a8a796dead187bc76b7669
OlgaStam <newrealityforme@gmail.com>
2023-08-15 15:43:22 +0300

    кнопки +-  - новый компонент  Quantity

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,38 +1,35 @@
-import {
-    Card,
-    Button,
-    CardActions,
-    CardContent,
-    TextField,
-} from '@mui/material'
+import { Button, Card, CardContent, TextField } from '@mui/material'
 import './ProductListItem.scss'
 import { useState } from 'react'
+import Quantity from 'components/Quantity/Quantity'
 
 type Props = {
     id: number
     title: string
     description: string
-    capacity: string
     type: string
+    capacity: string
     price: number
     image: string
-    addProductToCart: (count: number, id: number) => void
+    addProductToCart: (id: number, count: number) => void
 }
 
 const ProductListItem = ({
     id,
     title,
     description,
-    capacity,
     type,
+    capacity,
     price,
     image,
     addProductToCart,
 }: Props) => {
     const [count, setCount] = useState<number>(1)
+
     const onIncrementClick = () => {
         setCount((prevState) => prevState + 1)
     }
+
     const onDecrementClick = () => {
         setCount((prevState) => prevState - 1)
     }
@@ -43,40 +40,29 @@ const ProductListItem = ({
                 <div className="product-image">
                     <img src={image} alt={title} />
                 </div>
-                <h4 className="product-title">{title}</h4>
+                <h2 className="product-title">{title}</h2>
                 <p className="product-description">{description}</p>
                 <div className="product-features">Type: {type}</div>
-                <div className="product-features">Capacity: {capacity}Gb</div>
+                <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-price">
                     Price: <span>${price}</span>
                 </div>
-                <div className="product-quantity">
-                    <Button
-                        variant="outlined"
-                        onClick={onDecrementClick}
-                        disabled={count <= 1}
-                    >
-                        -
-                    </Button>
-                    <TextField value={count} size="small" />
-                    <Button
-                        variant="outlined"
-                        onClick={onIncrementClick}
-                        disabled={count >= 10}
-                    >
-                        +
-                    </Button>
-                </div>
-                <CardActions className="btns-wrap">
+                <Quantity
+                    count={count}
+                    onDecrementClick={onDecrementClick}
+                    onIncrementClick={onIncrementClick}
+                />
+                <div className="btns-wrap">
                     <Button
                         variant="outlined"
                         onClick={() => addProductToCart(id, count)}
                     >
                         Add to cart
                     </Button>
-                </CardActions>
+                </div>
             </CardContent>
         </Card>
     )
 }
+
 export default ProductListItem
