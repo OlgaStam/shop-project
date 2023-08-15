2af2527ec684eea40884b855bd01913a16699132
OlgaStam <newrealityforme@gmail.com>
2023-08-15 09:54:08 +0300

    CartPage - добавляем товары

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -1,9 +1,51 @@
-type Props = {}
+import { Typography } from '@mui/material'
+import productsArray, { Product, getProductsObject } from 'utils/productsArray'
 
-const CartPage = (props: Props) => {
-  return (
-    <div>CartPage</div>
-  )
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+}
+type productsObjectType = {
+    [id: number]: Product
+}
+const CartPage = ({ productsInCart }: Props) => {
+    const productsObject: productsObjectType = getProductsObject(productsArray)
+
+    return (
+        <div>
+            <div>
+                <Typography
+                    variant="h4"
+                    component="h1"
+                    sx={{
+                        margin: '30px 0',
+                    }}
+                >
+                    Cart
+                </Typography>
+            </div>
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[parseInt(productId)].title}:{' '}
+                        {productsInCart[parseInt(productId)]} : ${' '}
+                        {productsObject[parseInt(productId)].price}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total: ${' '}
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsObject[parseInt(productId)].price *
+                            productsInCart[parseInt(productId)],
+                    0
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default CartPage
