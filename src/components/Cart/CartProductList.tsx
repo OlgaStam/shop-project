cfa299eca11894adeb7282bca6ab05e300ab3e08
OlgaStam <newrealityforme@gmail.com>
2023-08-15 11:15:10 +0300

    CartProductList в КартХедер

---
+++ src/components/Cart/CartProductList.tsx
@@ -0,0 +1,28 @@
+import productsArray, { Product, getProductsObject } from 'utils/productsArray'
+
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+}
+
+type productsObjectType = {
+    [id: number]: Product
+}
+
+const CartProductList = ({productsInCart}: Props) => {
+    const productsObject: productsObjectType = getProductsObject(productsArray)
+   
+    return (
+        <div>
+            {Object.keys(productsInCart).map((productId) => (
+                <div key={productId}>
+                    {productsObject[parseInt(productId)].title}:{' '}
+                    {productsInCart[parseInt(productId)]}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default CartProductList
--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,32 +1,15 @@
+import CartProductList from 'components/Cart/CartProductList'
 import CartTotal from 'components/Cart/CartTotal'
-import productsArray, { Product, getProductsObject } from 'utils/productsArray'
-// console.log(productsArray)
 type Props = {
     productsInCart: {
         [id: number]: number
     }
 }
-//ненадежно привязываться к порядковому номеру в массиве как к уникальному
-// перепишем массив в объект, в котором ключом будет айди, с помощью функции в utils/productsArray
-
-// теперь экспорт из utils/productsArray ф-ю и тип
-
-type productsObjectType = {
-    [id: number]: Product
-}
 const CartHeader = ({ productsInCart }: Props) => {
-    const productsObject: productsObjectType = getProductsObject(productsArray)
 
     return (
         <div>
-            <div>
-                {Object.keys(productsInCart).map((productId) => (
-                    <div key={productId}>
-                        {productsObject[parseInt(productId)].title}:{' '}
-                        {productsInCart[parseInt(productId)]}
-                    </div>
-                ))}
-            </div>
+            <CartProductList productsInCart={productsInCart} />
             <CartTotal productsInCart={productsInCart} />
         </div>
     )
