01e866e57debdd924f3e6db1bf06d78cba6bcb1e
OlgaStam <newrealityforme@gmail.com>
2023-08-17 20:06:40 +0300

    описали ф-ю редьюса

--- src/container/redux/likeReducer.ts
+++ src/container/redux/likeReducer.ts
@@ -10,7 +10,20 @@ const initialState: State = {
 }
 
 const likeReducer = (state = initialState, action: AnyAction) => {
-    return state
+    switch (action.type) {
+        case 'ADD_LIKE':
+            return {
+                ...state,
+                [action.id]: true,
+            }
+        case 'REMOVE_LIKE':
+            return {
+                ...state,
+                [action.id]: false,
+            }
+        default:
+            return state
+    }
 }
 
 export default likeReducer
