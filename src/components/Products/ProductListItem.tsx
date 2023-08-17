811d8fefb0e82485a092ed835ae9a58834ad9618
OlgaStam <newrealityforme@gmail.com>
2023-08-17 20:33:02 +0300

    упростили ф-ю

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -2,9 +2,9 @@ import { Button, Card, CardContent } from '@mui/material'
 import './ProductListItem.scss'
 import { useState } from 'react'
 import Quantity from 'components/Quantity/Quantity'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useAppDispatch, useAppSelector } from 'container/redux/hooks';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import { useAppDispatch, useAppSelector } from 'container/redux/hooks'
 
 type Props = {
     id: number
@@ -42,48 +42,43 @@ const ProductListItem = ({
     return (
         <Card variant="outlined">
             <CardContent>
-                <Button variant='outlined' onClick={() => {
-                    if (isLiked) {
-                        dispatch({
-                            type: "REMOVE_LIKE",
-                            id,
-                        })
-                    } else {
-                        dispatch({
-                            type: "ADD_LIKE",
-                            id,
-                        })
-                    }
-                }
-                }>
-                {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-            </Button>
-            <div className="product-image">
-                <img src={image} alt={title} />
-            </div>
-            <h2 className="product-title">{title}</h2>
-            <p className="product-description">{description}</p>
-            <div className="product-features">Type: {type}</div>
-            <div className="product-features">Capacity: {capacity} Gb</div>
-            <div className="product-price">
-                Price: <span>${price}</span>
-            </div>
-            <Quantity
-                count={count}
-                onDecrementClick={onDecrementClick}
-                onIncrementClick={onIncrementClick}
-                minCount={1}
-            />
-            <div className="btns-wrap">
                 <Button
                     variant="outlined"
-                    onClick={() => addProductToCart(id, count)}
+                    onClick={() => {
+                        dispatch({
+                            type: 'TOGGLE_LIKE',
+                            id,
+                        })
+                    }}
                 >
-                    Add to cart
+                    {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
                 </Button>
-            </div>
-        </CardContent>
-        </Card >
+                <div className="product-image">
+                    <img src={image} alt={title} />
+                </div>
+                <h2 className="product-title">{title}</h2>
+                <p className="product-description">{description}</p>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity} Gb</div>
+                <div className="product-price">
+                    Price: <span>${price}</span>
+                </div>
+                <Quantity
+                    count={count}
+                    onDecrementClick={onDecrementClick}
+                    onIncrementClick={onIncrementClick}
+                    minCount={1}
+                />
+                <div className="btns-wrap">
+                    <Button
+                        variant="outlined"
+                        onClick={() => addProductToCart(id, count)}
+                    >
+                        Add to cart
+                    </Button>
+                </div>
+            </CardContent>
+        </Card>
     )
 }
 
