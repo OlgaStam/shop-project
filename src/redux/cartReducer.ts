8c2932a471eec15465eb7a9308b4a88930fc6b37
OlgaStam <newrealityforme@gmail.com>
2023-08-22 22:01:29 +0300

    создали  add-product-to-cart

--- src/redux/cartReducer.ts
+++ src/redux/cartReducer.ts
@@ -1,9 +1,20 @@
-import { AnyAction } from "@reduxjs/toolkit"
+import { AnyAction } from '@reduxjs/toolkit'
 
 type CartStateType = { [id: number]: number }
 
 const initialStat: CartStateType = {
-  1:5, 2:5
+    1: 5,
+    2: 5,
 }
-const cartReducer= (state= initialStat, action : AnyAction) => state
+const cartReducer = (state = initialStat, action: AnyAction) => {
+    switch (action.type) {
+        case 'add-product-to-cart':
+            return {
+                ...state,
+                [action.id]: (state[action.id] || 0) + action.count,
+            }
+            default:
+              return state
+    }
+}
 export default cartReducer
